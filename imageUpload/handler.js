7d7403beeb128eafdc620f28021583aa46c70c6f
Ishmael <mudauishmael@gmail.com>
2021-05-15 10:57:49 +0200

    add authentication
    Token authentication to all calls.

--- imageUpload/handler.js
+++ imageUpload/handler.js
@@ -1,26 +1,89 @@
 'use strict';
 
+const jwt = require('jsonwebtoken');
+
+// Passed from `environment` of serverless.yml
+const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
+const AUTH0_CLIENT_PUBLIC_KEY = process.env.AUTH0_CLIENT_PUBLIC_KEY;
+
 var AWS = require('aws-sdk');
 
-var s3 = new AWS.S3();
-var params = JSON.parse(event.body);
-
-var s3Params = {
-  Bucket: 'slsupload',
-  Key:  params.name,
-  ContentType: params.type,
-  ACL: 'public-read',
+// helper function
+const generatePolicy = (principalId, effect, resource) => {
+  const authResponse = {};
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    const policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    const statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
 };
 
-// var uploadURL = s3.getSignedUrl('putObject', s3Params);
+// Authorizer function field in serverless.yml
+// To authenticate calls before processing. 
+/**
+ * 
+ * @param {*} event 
+ * @param {*} context 
+ * @param {*} callback 
+ * returns auth token
+ */
 
+module.exports.auth = (event, context, callback) => {
+  console.log('event', event);
+  if (!event.authorizationToken) {
+    return callback('Unauthorized');
+  }
 
+  const tokenParts = event.authorizationToken.split(' ');
+  const tokenValue = tokenParts[1];
+
+  if (!(tokenParts[0].toLowerCase() === 'bearer' && tokenValue)) {
+    // no auth token!
+    return callback('Unauthorized');
+  }
+  const options = {
+    audience: AUTH0_CLIENT_ID,
+  };
+
+  try {
+    jwt.verify(tokenValue, AUTH0_CLIENT_PUBLIC_KEY, options, (verifyError, decoded) => {
+      if (verifyError) {
+        console.log('verifyError', verifyError);
+        // 401 Unauthorized
+        console.log(`Token invalid. ${verifyError}`);
+        return callback('Unauthorized');
+      }
+      // is custom authorizer function
+      console.log('valid from customAuthorizer', decoded);
+      return callback(null, generatePolicy(decoded.sub, 'Allow', event.methodArn));
+    });
+  } catch (err) {
+    console.log('catch error. Invalid token', err);
+    return callback('Unauthorized');
+  }
+};
+
+/**
+ * 
+ * @param {*} event 
+ * @param {*} context 
+ * @param {*} callback 
+ *  Stores image in s3 bucket
+ */
 module.exports.requestUploadURL = (event, context, callback) => {
   var s3 = new AWS.S3();
   var params = JSON.parse(event.body);
 
   var s3Params = {
-    Bucket: 'slsupload',
+    Bucket: 'tweakImages',
     Key:  params.name,
     ContentType: params.type,
     ACL: 'public-read',
@@ -31,12 +94,11 @@ module.exports.requestUploadURL = (event, context, callback) => {
   callback(null, {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': 'https://www.my-site.com'
+      'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({ uploadURL: uploadURL }),
   })
 }
-
 
 
 module.exports.hello = async (event) => {
